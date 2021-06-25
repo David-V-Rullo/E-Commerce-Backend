367fc3bb7aa9926f22e8f21b9f26faea5dbe7719
David Rullo <d.v.rullo@gmail.com>
2021-06-25 16:47:12 -0400

    Finall have models finished, Working on Routes now

--- models/Product.js
+++ models/Product.js
@@ -8,8 +8,40 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  //First Object defines columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
