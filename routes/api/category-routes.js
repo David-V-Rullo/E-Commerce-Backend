bd7b6431cb95b23a3e6e925eb246f487c724f72c
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-29 19:25:21 -0400

    Final push with working app

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -78,7 +78,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(categoryData);
+    res.status(200).json(categoryData, { message: 'Category Deleted Successfully'});
   } catch (err) {
     res.status(500).json(err);
   }
